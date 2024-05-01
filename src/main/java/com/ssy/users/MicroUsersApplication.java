f0d7e7e14465199e71ac9b0c5e213edbefc9b233
Sidy SY <sidysy2013@gmail.com>
2024-05-01 14:41:43 +0200

    Initial commit

---
+++ src/main/java/com/ssy/users/MicroUsersApplication.java
@@ -0,0 +1,13 @@
+package com.ssy.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroUsersApplication.class, args);
+    }
+
+}
