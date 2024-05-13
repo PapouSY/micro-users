a5b003a63ba6490314b50ac08ac0ddad01af4257
Sidy SY <sidysy2013@gmail.com>
2024-05-13 12:08:18 +0200

    security 3

--- src/main/java/com/ssy/users/MicroUsersApplication.java
+++ src/main/java/com/ssy/users/MicroUsersApplication.java
@@ -1,5 +1,10 @@
 package com.ssy.users;
 
+import com.ssy.users.entities.Role;
+import com.ssy.users.entities.User;
+import com.ssy.users.service.UserService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class MicroUsersApplication {
 
+    @Autowired
+    UserService userService;
     public static void main(String[] args) {
         SpringApplication.run(MicroUsersApplication.class, args);
     }
@@ -17,4 +24,24 @@ public class MicroUsersApplication {
         return new BCryptPasswordEncoder();
     }
 
+
+   /* @PostConstruct
+    void init_users(){
+        // ajout roles
+        userService.addRole(new Role(null,"ADMIN"));
+        userService.addRole(new Role(null,"USER"));
+
+        //ajout users
+        userService.saveUser(new User(null,"admin","123",true,null));
+        userService.saveUser(new User(null,"sidy","123",true,null));
+        userService.saveUser(new User(null,"ssy","123",true,null));
+
+        //ajout role to users
+
+        userService.addRoleToUser("admin","ADMIN");
+        userService.addRoleToUser("admin","USER");
+        userService.addRoleToUser("sidy","USER");
+        userService.addRoleToUser("ssy","USER");
+
+    }*/
 }
