23551e5eeed129874d447e2926f947aedec92253
Sidy SY <sidysy2013@gmail.com>
2024-11-10 13:47:08 +0100

    security

--- src/main/java/com/ssy/users/security/SecParams.java
+++ src/main/java/com/ssy/users/security/SecParams.java
@@ -5,4 +5,5 @@ public interface SecParams {
     public static final String  SECRET = "sidysy2013@gmail.com";
 
     public static final long EXP_TIME = 864000000;
+    public static final String PREFIX = "Bearer ";
 }
